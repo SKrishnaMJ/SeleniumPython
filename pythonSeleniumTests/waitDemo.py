8d219f767a30fa8dd42ea1f7e6b4b6674c12d778
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2024-06-24 12:15:37 -0500

    added explicit wait to the e2e test here

--- pythonSeleniumTests/waitDemo.py
+++ pythonSeleniumTests/waitDemo.py
@@ -2,7 +2,9 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(3)
@@ -15,7 +17,18 @@ assert len(products) > 0
 for product in products:
     # This is the concept of chaining in selenium, we find element on an already found element
     product.find_element(By.XPATH,'div/button').click()
-    time.sleep(3)
+
+driver.find_element(By.CSS_SELECTOR, 'img[alt="Cart"]').click()
+driver.find_element(By.XPATH,'//button[text()="PROCEED TO CHECKOUT"]').click()
+driver.find_element(By.CSS_SELECTOR, '.promoCode').send_keys("rahulshettyacademy")
+driver.find_element(By.CSS_SELECTOR, '.promoBtn').click()
+
+# we are using the explicit wait by importing the class WebDriverWait() and creating its object
+wait = WebDriverWait(driver, 10)
+
+# Here we wait until the element is visible on the screen, be privy of brackets inside presence_of_element
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR,'.promoInfo')))
+print(driver.find_element(By.CSS_SELECTOR,'.promoInfo').text)
 
 
 
