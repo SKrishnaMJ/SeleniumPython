a5e66ce916fc403ef0ecb5e7c4a13ee67b1cdd2b
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2024-06-27 16:31:36 -0500

    Executing a javascript command on the browser using the driver.execute_script()

---
+++ pythonSeleniumTests/javascriptExecute.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("--ignore-certificate-errors")
+
+driver = webdriver.Chrome(options=chromeOptions)
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+
+# this below command is used to execute a javscript command on the browser
+driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+driver.get_screenshot_as_file("screenshot1.png")
+
+
+driver.close()
+
