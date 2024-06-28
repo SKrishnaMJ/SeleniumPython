83f07711975476b1628c6bbdb60da44c17135284
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2024-06-28 11:41:45 -0500

    End to End Test

---
+++ pythonSeleniumTests/e2eCompleteTest.py
@@ -0,0 +1,38 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument("--ignore-certificate-errors")
+
+driver = webdriver.Chrome(options=chromeOptions)
+driver.implicitly_wait(6)
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+driver.find_element(By.LINK_TEXT, "Shop").click()
+wait = WebDriverWait(driver,20)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, '.btn-primary')))
+time.sleep(3)
+items = driver.find_elements(By.CSS_SELECTOR, "app-card")
+
+for item in items:
+    item.find_element(By.CSS_SELECTOR,"div button").click()
+
+driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+driver.find_element(By.CSS_SELECTOR, ".btn-success").click()
+driver.find_element(By.CSS_SELECTOR, ".checkbox").click()
+
+driver.find_element(By.ID, 'country').send_keys("ind")
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, '.suggestions ul li a')))
+countries = driver.find_elements(By.CSS_SELECTOR, '.suggestions ul li a')
+for country in countries:
+    print(country.text)
+    if(country.text == "India"):
+        country.click()
+
+time.sleep(3)
+driver.close()
