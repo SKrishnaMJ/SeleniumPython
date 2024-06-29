b5423e6332d19bd1f4f22ec7d6c631b58a57f418
SKrishnaMJ <146481712+SKrishnaMJ@users.noreply.github.com>
2024-06-29 11:45:12 -0500

    added two more demo tests to test -k flag while running the test from command line

--- pytestFramework/test_Demo.py
+++ pytestFramework/test_Demo.py
@@ -1,2 +1,7 @@
 def test_Firsttest():
     print("Yo")
+
+def test_ProdMath():
+    a=2
+    b=20
+    assert a*b == 40, "Product no match #BadMath"
--- pytestFramework/test_Demo2.py
+++ pytestFramework/test_Demo2.py
@@ -1,3 +1,8 @@
 def test_Greet():
     msg = "Hello"
     assert msg == "Hi", "There is no match"
+
+def test_SumMath():
+    a=6
+    b=4
+    assert a+b == 10, "Sum is not a match #BadMath"
